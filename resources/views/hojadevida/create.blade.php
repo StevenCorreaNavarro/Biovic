9925aaaa28e86b20e0800a0bc2e532ba3379bd2d
josemeneses88 <josemeneses@unicomfacauca.edu.co>
2025-03-05 17:18:02 -0500

    incluir quipo y modelo

--- resources/views/hojadevida/create.blade.php
+++ resources/views/hojadevida/create.blade.php
@@ -11,7 +11,7 @@
 @extends('layouts.header')
 
 {{-- 17- Creacion de formulario de envio --}}
-<h1>HOLA DESDE CREATE</h1>
+<h1>HOLA DESDE CREATE 2</h1>
 {{-- desde aqui se envia a storage, con el uso del methoso post--}}
 <form action="{{url('/hojadevida')}}" method="post" enctype="multipart/form-data">
     @csrf {{-- LLave de seguridad obligatoria --}}
