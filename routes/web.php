0020761986e2f98d4b10c61c63854bcb98ddcef4
josemeneses88 <josemeneses@unicomfacauca.edu.co>
2025-03-05 15:28:34 -0500

    CRUD Hoja de vida

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ use Illuminate\Support\Facades\Route;
 use RealRashid\SweetAlert\Facades\Alert;
 use Spatie\FlareClient\View;
 
+use App\Http\Controllers\HojadevidaController;
+
+
+Route::resource('hojadevida',HojadevidaController::class);
+// acceder a show
+Route::get('/hojadevida/{id}/show', [HojadevidaController::class, 'show'])->name('hojadevida.show');
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
