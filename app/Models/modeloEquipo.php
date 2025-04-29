a5de7f92dd0bb8790278d75db34353b613abf308
josemeneses88 <josemeneses@unicomfacauca.edu.co>
2025-04-29 08:40:11 -0500

    solucion errores modeloEquipo - User copy

--- app/Models/modeloEquipo.php
+++ app/Models/modeloEquipo.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class nombreEquipo extends Model
-{
+class modeloEquipo extends Model{ // error en como estaba llamada la class
     use HasFactory;
 
     public function hojadevidas(){
